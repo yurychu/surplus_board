d581cf4736bbc5ee9801abd11b6310872b513f47
yurychu <tesewar@mail.ru>
2017-01-03 21:57:18 +0300

    unittest added

--- surplus_board/functional_tests.py
+++ surplus_board/functional_tests.py
@@ -1,6 +1,22 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome('/home/yurychu/programs/chromedriver')
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome('/home/yurychu/programs/chromedriver')
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_adding_a_list_items_and_retireve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Surplus Board', self.browser.title)
+
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
