00b90cb7b26fc36a8318120c9488d07a70c873e9
yurychu <tesewar@mail.ru>
2017-01-06 19:16:27 +0300

    base url mapping

--- surplus_board/surplus_board/urls.py
+++ surplus_board/surplus_board/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+# from django.contrib import admin
+
+from boards import views
+
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
+
 ]
